bd267e98a31da8afba5ab3c92bb9d67fe84c5936
andrea <andrearr2412@gmail.com>
2021-06-23 15:20:23 -0400

    Update 02_clean.nyc_complaints.R

--- 02_clean.nyc_complaints.R
+++ 02_clean.nyc_complaints.R
@@ -1,11 +1,12 @@
+###load data 
 nyc_clean <- read_csv("CCRB%20Complaint%20Database%20Raw%2004.20.2021.csv")
 
-
+###Change Date colombs into date format
+nyc_clean <- nyc_clean %>%
 mutate(ReceivedDate = mdy(ReceivedDate), 
        IncidentDate = mdy(IncidentDate), 
-       CloseDate = mdy(CloseDate))
+       CloseDate = mdy(CloseDate)) %>%
+  filter(ReceivedDate >= '2007-01-01' & ReceivedDate <= '2017-12-31')
 
-complaints.filtered <- complaints %>% filter(ReceivedDate >= '2007-01-01' & ReceivedDate <= '2017-12-31')
-
-
-save(trips, weather, file='trips.RData')
+### save nyc_clean
+save(nyc_clean, file='nyc_clean.RData')
